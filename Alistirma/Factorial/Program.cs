b3c0c5abb52a647f38023aa674979e7bfaa1331e
Fatih Ceylan <fceylan001@gmail.com>
2021-10-06 17:33:59 +0300

    Add files via upload

---
+++ Alistirma/Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz: ");
+            while (!Int32.TryParse(Console.ReadLine(), out int number))
+            {
+                Console.WriteLine("Lütfen sadece sayı giriniz");
+            }
+            int sayi = int.Parse(Console.ReadLine());
+            Console.WriteLine(sayi + " " + "Sayısının faktöryeli: " + factorial_WhileLoop(sayi));
+            Console.ReadLine();
+        }
+        public static double factorial_WhileLoop(int number)
+        {
+            double result = 1;
+            while (number != 1)
+            {
+                result = result * number;
+                number = number - 1;
+            }
+            return result;
+        }
+    }
+}
